250c013e9585ecbec6a0b3ac110cd471d4f7c338
kendricktan <kendricktan0814@gmail.com>
2015-06-28 19:54:06 +1000

    reliable tcp communication examples

--- tcp/client.py
+++ tcp/client.py
@@ -1,16 +1,20 @@
-import socket
+import socket, sys
 
+if len(sys.argv) > 1:
+    if sys.argv[1] == '-h' or sys.argv[1] == '--help':
+        print 'Usage: python client.py [target ip address]'
+        sys.exit()
 
-TCP_IP = '127.0.0.1'
+MSG = ''
+TCP_IP = str(sys.argv[1]) if len(sys.argv) > 1 else '127.0.0.1'
 TCP_PORT = 5005
-BUFFER_SIZE = 32
-MESSAGE = ""
+BUFFER_SIZE = 32 # For fast response
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((TCP_IP, TCP_PORT))
 
-while '/quit' not in MESSAGE:
-    MESSAGE = raw_input()
-    s.send(MESSAGE)
+while '/quit' not in MSG:
+    MSG = raw_input()
+    s.send(MSG)
 
 s.close()
--- tcp/server.py
+++ tcp/server.py
@@ -1,31 +1,29 @@
 import socket
+import SocketServer
 
-SERVER_LISTEN = True
+# Allows reusing address
+SocketServer.ThreadingTCPServer.allow_reuse_address = True
+
+SERVER_RUNNING = True
 TCP_IP = '127.0.0.1'
 TCP_PORT = 5005
-BUFFER_SIZE = 32 # For fast response
+BUFFER_SIZE = 32  # For fast response
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((TCP_IP, TCP_PORT))
 s.listen(1)
 
 conn, addr = s.accept()
-
-print 'Connection address: ', addr
-
-data = None
-
-while SERVER_LISTEN:
+while SERVER_RUNNING:
     data = conn.recv(BUFFER_SIZE)
-
-    # Don't wanna just receive null data...
     while not data:
         data = conn.recv(BUFFER_SIZE)
 
     if '/quit' in data:
-        print 'Received kill message...'
-        SERVER_LISTEN = False
+        SERVER_RUNNING = False
 
     print data
+
+    # Add moving robocup arm here
 
 conn.close()
