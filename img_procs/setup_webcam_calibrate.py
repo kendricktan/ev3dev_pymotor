918ed6045810bbcc9d8a3ab0002501c776874430
Kendrick Tan <kendricktan0814@gmail.com>
2015-07-05 13:27:35 +0000

    fixed green filter

--- img_procs/setup_webcam_calibrate.py
+++ img_procs/setup_webcam_calibrate.py
@@ -16,7 +16,7 @@ from settings import *
 # boundary for green filter
 GREEN_ROI = 100
 GREEN_RANGE  = [([0, 75, 0], [60, 125, 60])] # Increase B(G)R [G] range if detects fuzzy lines
-GREEN_THRESH = 25 # Lower if can't detect, higher if detects too many
+GREEN_THRESH = 15 # Lower if can't detect, higher if detects too many
 GREEN_AREA_MAX = 80000
 GREEN_AREA_MIN = 200
 
@@ -50,43 +50,26 @@ while True:
         mask = cv2.inRange(ROI_g, lower, upper)
         ROI_g = cv2.bitwise_and(ROI_g, ROI_g, mask=mask)
 
-    # Convert to grayscale
     ROI_g = cv2.cvtColor(ROI_g, cv2.COLOR_BGR2GRAY)
+    ret, ROI_g = cv2.threshold(ROI_g, THRESH, 255, 0)
 
-    ret, ROI_g = cv2.threshold(ROI_g, 0, 255, 0)
-
-    cv2.bitwise_not(ROI_g, ROI_g)
-
-    # Reduces noise in image and dilate to increase white region (since its negative)
-    erode_e = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3));
-    dilate_e = cv2.getStructuringElement(cv2.MORPH_RECT, (5, 5));
-
-    cv2.erode(ROI_g, erode_e)
-    cv2.dilate(ROI_g, dilate_e)
-
-    # Find contours
-    ROI_g = cv2.Canny(ROI_g, 100, 200)
-
-    contours, hierarchy = cv2.findContours(ROI_g, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-
-    c = 0
+    # Finds contours
+    contours, hierarchy = cv2.findContours(ROI_g.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
 
     for i in contours:
         area = cv2.contourArea(i)
         moments = cv2.moments(i)
 
-        if area > GREEN_AREA_MIN:
-            if moments['m00'] != 0.0:
-                if moments['m01'] != 0.0:
-                    c = c + 1
-                    cx = int(moments['m10']/moments['m00'])
-                    cy = int(moments['m01']/moments['m00'])#+GREEN_ROI
+        # We only want to get an area of > the threshold to prevent not usable contours
+        if area>AREA_THRESH:
+            if moments['m00']!=0.0:
+                if moments['m01']!=0.0:
+                                                                                                                    # We can calculate the centroid coordinates using this
+                    cx = int(moments['m10']/moments['m00'])         # cx = M10/M00
+                    cy = int(moments['m01']/moments['m00'])         # cy = M01/M00
 
-                    cv2.putText(frame, 'Area: ' + str(area) + '; x: ' + str(cx) + ' y: ' + str(cy), (10, 25+(c*10)), cv2.FONT_HERSHEY_PLAIN, 1,   (255, 0, 0), 2)
-
-                    cv2.circle(frame, (cx, cy), 8, BLUE_COLOR, -1)
-
+                    cv2.circle(frame, (cx, cy), 4, BLUE_COLOR, -1)
 
     # Overflow protection
     if counter >= sys.maxint - 100000:
@@ -94,8 +77,8 @@ while True:
 
     # Display GUI for captured frame
     if frame is not None:
+        #cv2.imshow('pi camera', frame)
         cv2.imshow('pi camera', frame)
-        #cv2.imshow('pi camera', ROI_g)
 
     # Debug
     if cv2.waitKey(1) & 0xFF == ord('d'):
