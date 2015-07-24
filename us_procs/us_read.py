448cadd4b102d429c2f7e2890e383ead41a93e38
Kendrick Tan <kendricktan0814@gmail.com>
2015-07-24 13:47:04 +0000

    added ultrasonic driver

---
+++ us_procs/us_read.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python
+
+import time
+import RPi.GPIO as GPIO
+# remember to change the GPIO values below to match your sensors
+# GPIO output = the pin that's connected to "Trig" on the sensor
+# GPIO input = the pin that's connected to "Echo" on the sensor
+
+class us_driver:
+    def __init__(self, GPIO_trig, GPIO_echo):
+        # Disable warnings
+        GPIO.setwarnings(False)
+
+        # Uses the value of GPIO pins (e.g. GPIO 14 @ port 8 = 14)
+        GPIO.setmode(GPIO.BCM)
+
+        # GPIO output = trig
+        # GPIO input = echo
+        GPIO.setup(GPIO_trig, GPIO.OUT)
+        GPIO.setup(GPIO_echo, GPIO.IN)
+        GPIO.output(GPIO_trig, GPIO.LOW)
+
+        self.GPIO_trig = GPIO_trig
+        self.GPIO_echo = GPIO_echo
+
+        time.sleep(0.03)
+
+    def read(self):
+        GPIO.output(self.GPIO_trig, True)
+        time.sleep(0.00001) # waits for 10Us (sensor expects)
+
+        GPIO.output(self.GPIO_trig, False)
+
+        while GPIO.input(self.GPIO_echo) == 0:
+            signaloff = time.time()
+
+        while GPIO.input(self.GPIO_echo) == 1:
+            signalon = time.time()
+
+        timepassed = signalon - signaloff
+
+        return timepassed * 17000 # in cm
+
+
+us_sens01 = us_driver(14, 15)
+
+for x in range(0, 100):
+    print us_sens01.read()#!/usr/bin/python
+    time.sleep(0.01)
+
